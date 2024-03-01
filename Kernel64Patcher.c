6c2b8dc066c9720a9448e35029a5006b3c6e33bf
y08wilm <152432414+y08wilm@users.noreply.github.com>
2024-03-01 17:55:34 -0600

    vm_fault_enter

--- Kernel64Patcher.c
+++ Kernel64Patcher.c
@@ -285,7 +285,7 @@ int get_vm_fault_enter_patch_ios8(void* kernel_buf,size_t kernel_len) {
         return -1;
     }
     printf("%s: Found \"vm_fault_enter\" patch loc at %p\n",__FUNCTION__,GET_OFFSET(kernel_len,ent_loc));
-    addr_t xref_stuff = (addr_t)find_next_insn_matching_64(0, kernel_buf, kernel_len, GET_OFFSET(kernel_len,ent_loc, insn_is_tbnz_w32);
+    addr_t xref_stuff = (addr_t)find_next_insn_matching_64(0, kernel_buf, kernel_len, ent_loc, insn_is_tbnz_w32);
     if(!xref_stuff) {
         printf("%s: Could not find \"vm_fault_enter\" xref\n",__FUNCTION__);
         return -1;
@@ -294,7 +294,7 @@ int get_vm_fault_enter_patch_ios8(void* kernel_buf,size_t kernel_len) {
     printf("%s: Found \"vm_fault_enter\" xref at %p\n\n", __FUNCTION__,(void*)(xref_stuff));
     printf("%s: Patching \"vm_fault_enter\" at %p\n\n", __FUNCTION__,(void*)(xref_stuff))
     // 0x5280002d is mov x13, #0x1
-    *(uint32_t *) (kernel_buf + xref_stuff) = 0x5280002d;
+    *(uint32_t *) *(kernel_buf + xref_stuff) = 0x5280002d;
     return 0;
 }
 
