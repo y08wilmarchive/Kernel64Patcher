feaeae549034ff9eb2561347b501cf802cfeeaaf
y08wilm <152432414+y08wilm@users.noreply.github.com>
2024-03-11 10:49:23 -0500

    test

--- Kernel64Patcher.c
+++ Kernel64Patcher.c
@@ -392,7 +392,7 @@ int get__MKBDeviceUnlockedSinceBoot_patch_ios8(void* kernel_buf,size_t kernel_le
         return -1;
     }
     printf("%s: Found \"_MKBDeviceUnlockedSinceBoot\" patch loc at %p\n",__FUNCTION__,GET_OFFSET(kernel_len,ent_loc));
-    addr_t xref_stuff = (addr_t)find_last_insn_matching_64(0, kernel_buf, kernel_len, ent_loc, insn_is_bl_64);
+    void* xref_stuff = find_last_insn_matching_64(0, kernel_buf, kernel_len, ent_loc, insn_is_bl_64);
     if(!xref_stuff) {
         printf("%s: Could not find \"_MKBDeviceUnlockedSinceBoot\" xref\n",__FUNCTION__);
         return -1;
