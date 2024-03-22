7857cd9167f896cc1054be1400867f5865a79dc2
y08wilm <152432414+y08wilm@users.noreply.github.com>
2024-03-22 17:07:04 -0500

    test

--- Kernel64Patcher.c
+++ Kernel64Patcher.c
@@ -727,7 +727,8 @@ int get_sandbox_patch_ios8(void* kernel_buf,size_t kernel_len) {
     uint32_t sb_evaluate_offset = get_sb_evaluate_offset(kernel_buf, kernel_len); // xref& bof64
     uint32_t *payloadAsUint32 = (uint32_t *)payload;
     uint32_t patchValue;
-    char* str = "_sep_panic_timer != NULL"; // virtual bool AppleSEPManager::start(IOService *)
+    //char* str = "_sep_panic_timer != NULL"; // virtual bool AppleSEPManager::start(IOService *)
+    char* str = "\"sks request timeout\"";
     void* ent_loc = memmem(kernel_buf, kernel_len, str, sizeof(str) - 1);
     if(!ent_loc) {
         printf("%s: Could not find \"virtual bool AppleSEPManager::start(IOService *)\" string\n",__FUNCTION__);
