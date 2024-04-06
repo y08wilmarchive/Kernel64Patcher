70de70a00a8c420863f48c83f413a3945d7e3461
y08wilm <152432414+y08wilm@users.noreply.github.com>
2024-04-06 16:30:02 -0500

    test

--- Kernel64Patcher.c
+++ Kernel64Patcher.c
@@ -1353,7 +1353,7 @@ int get_sandbox_patch_ios10(void* kernel_buf,size_t kernel_len) {
     uint32_t sb_evaluate_offset = get_sb_evaluate_offset_10(kernel_buf, kernel_len); // xref& bof64
     uint32_t *payloadAsUint32 = (uint32_t *)payload;
     uint32_t patchValue;
-    char* str = "\"sks request timeout\"";
+    char* str = "AppleKeyStore: sks timeout strike %d\n";
     void* ent_loc = memmem(kernel_buf, kernel_len, str, sizeof(str) - 1);
     if(!ent_loc) {
         printf("%s: Could not find \"sks request timeout\" string\n",__FUNCTION__);
