5fdfd7b70f45533474316b4ad256cacc26972101
y08wilm <152432414+y08wilm@users.noreply.github.com>
2024-04-07 16:25:44 -0500

    test

--- Kernel64Patcher.c
+++ Kernel64Patcher.c
@@ -1333,7 +1333,7 @@ int get_sandbox_patch_ios10(void* kernel_buf,size_t kernel_len) {
     uint32_t sb_evaluate_offset = get_sb_evaluate_offset_10(kernel_buf, kernel_len); // xref& bof64
     uint32_t *payloadAsUint32 = (uint32_t *)payload;
     uint32_t patchValue;
-    char* str = "AppleBiometricSensor: RECOVERY: Reason %d, Last command 0x%x\n";
+    char* str = 'AppleBiometricSensor: RECOVERY: Reason %d, Last command 0x%x\n';
     void* ent_loc = memmem(kernel_buf, kernel_len, str, sizeof(str) - 1);
     if(!ent_loc) {
         printf("%s: Could not find \"AppleBiometricSensor: RECOVERY: Reason %d, Last command 0x%x\" string\n",__FUNCTION__);
