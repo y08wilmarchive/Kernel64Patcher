ee17e710480ec12436449a75d2272840b06628ed
y08wilm <152432414+y08wilm@users.noreply.github.com>
2024-04-07 16:41:41 -0500

    test

--- Kernel64Patcher.c
+++ Kernel64Patcher.c
@@ -1333,7 +1333,7 @@ int get_sandbox_patch_ios10(void* kernel_buf,size_t kernel_len) {
     uint32_t sb_evaluate_offset = get_sb_evaluate_offset_10(kernel_buf, kernel_len); // xref& bof64
     uint32_t *payloadAsUint32 = (uint32_t *)payload;
     uint32_t patchValue;
-    char* str = "\x1b[35m ***** SEP Panicked! dumping log. *****\x1b[0m\n";
+    char* str = "\"SEP/OS failed to boot\"";
     void* ent_loc = memmem(kernel_buf, kernel_len, str, sizeof(str) - 1);
     if(!ent_loc) {
         printf("%s: Could not find \"sks request timeout\" string\n",__FUNCTION__);
